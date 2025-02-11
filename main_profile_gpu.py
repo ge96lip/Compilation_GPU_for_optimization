c1973fbf7a4a11f19efad419e740cdadba3594d1
rmaresh-uw <120285839+rmaresh-uw@users.noreply.github.com>
2025-02-11 09:17:51 +0100

    cuda

--- main_profile_gpu.py
+++ main_profile_gpu.py
@@ -61,7 +61,7 @@ def run_performance_test(grid_sizes, num_iterations=1000):
 
 def run_profile():
     # Define grid sizes
-    grid_sizes = [10, 20, 50, 100] # could include 200
+    grid_sizes = [10, 20, 50, 100 ,200, 500, 1000, 2000] #, 5000] # could include 200
 
     # Run performance test
     performance_data = run_performance_test(grid_sizes)
